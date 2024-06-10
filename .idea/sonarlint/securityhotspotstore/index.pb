a3f0f667dda3608473e2c14c8a7a2cd6b7b6a222
Adrian Weidig <70036819+adrianweidig@users.noreply.github.com>
2024-06-10 10:41:53 +0200

    JavaScript_Grundlagen Initialisierung
    Grundlegende Initialisierung der Grundstruktur. Wird permanent angepasst.

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+I
+JavaScript_Grundlagen.iml,e\0\e00484d876d1bc21bce6a128b6e1ba64b48cdb1a
+9
+	text.html,2\4\24bbb006a6564ab6d1f32e3604e76401b4b8dd93
+7
+text.js,a\3\a3211d25389241187fe2b2ede9fbab9e2b0c9b75
---
+++ ordnerstruktur_erstellen.ps1
@@ -0,0 +1,65 @@
+# Erstelle die Ordnerstruktur
+New-Item -Name "00_Uebungen" -ItemType "directory"
+New-Item -Name "01_Grundlagen" -ItemType "directory"
+New-Item -Name "02_Kontrollstrukturen" -ItemType "directory"
+New-Item -Name "03_OOP" -ItemType "directory"
+New-Item -Name "04_Funktionen" -ItemType "directory"
+New-Item -Name "05_Regulaere_Ausdruecke" -ItemType "directory"
+New-Item -Name "06_Ereignisse" -ItemType "directory"
+New-Item -Name "07_Speicher" -ItemType "directory"
+New-Item -Name "08_Promises" -ItemType "directory"
+New-Item -Name "09_Iteratoren" -ItemType "directory"
+New-Item -Name "10_Packages" -ItemType "directory"
+
+# Erstelle Unterordner in 01_Grundlagen
+New-Item -Path ".\01_Grundlagen" -Name "Syntax" -ItemType "directory"
+New-Item -Path ".\01_Grundlagen" -Name "Variablen" -ItemType "directory"
+New-Item -Path ".\01_Grundlagen" -Name "Datentypen" -ItemType "directory"
+
+# Erstelle Unterordner in 02_Kontrollstrukturen
+New-Item -Path ".\02_Kontrollstrukturen" -Name "Bedingungen" -ItemType "directory"
+New-Item -Path ".\02_Kontrollstrukturen" -Name "Ternary" -ItemType "directory"
+New-Item -Path ".\02_Kontrollstrukturen" -Name "For_Schleifen" -ItemType "directory"
+New-Item -Path ".\02_Kontrollstrukturen" -Name "While_Schleifen" -ItemType "directory"
+New-Item -Path ".\02_Kontrollstrukturen" -Name "Schleifensteuerung" -ItemType "directory"
+
+# Erstelle Unterordner in 03_OOP
+New-Item -Path ".\03_OOP" -Name "Klassen" -ItemType "directory"
+New-Item -Path ".\03_OOP" -Name "Vererbung" -ItemType "directory"
+
+# Erstelle Unterordner in 04_Funktionen
+New-Item -Path ".\04_Funktionen" -Name "Erstellung" -ItemType "directory"
+New-Item -Path ".\04_Funktionen" -Name "Aufruf" -ItemType "directory"
+New-Item -Path ".\04_Funktionen" -Name "Arrow" -ItemType "directory"
+New-Item -Path ".\04_Funktionen" -Name "Unterschied" -ItemType "directory"
+
+# Erstelle Unterordner in 05_Regulaere_Ausdruecke
+New-Item -Path ".\05_Regulaere_Ausdruecke" -Name "Einfuehrung" -ItemType "directory"
+New-Item -Path ".\05_Regulaere_Ausdruecke" -Name "Verwendung" -ItemType "directory"
+
+# Erstelle Unterordner in 06_Ereignisse
+New-Item -Path ".\06_Ereignisse" -Name "Handling" -ItemType "directory"
+New-Item -Path ".\06_Ereignisse" -Name "Haeufig" -ItemType "directory"
+
+# Erstelle Unterordner in 07_Speicher
+New-Item -Path ".\07_Speicher" -Name "Zuweisung" -ItemType "directory"
+New-Item -Path ".\07_Speicher" -Name "Garbage" -ItemType "directory"
+
+# Erstelle Unterordner in 08_Promises
+New-Item -Path ".\08_Promises" -Name "Einfuehrung" -ItemType "directory"
+New-Item -Path ".\08_Promises" -Name "Asynchron" -ItemType "directory"
+
+# Erstelle Unterordner in 09_Iteratoren
+New-Item -Path ".\09_Iteratoren" -Name "Verwendung" -ItemType "directory"
+New-Item -Path ".\09_Iteratoren" -Name "Generatoren" -ItemType "directory"
+
+# Erstelle Unterordner in 10_Packages
+New-Item -Path ".\10_Packages" -Name "React" -ItemType "directory"
+New-Item -Path ".\10_Packages" -Name "Vue.js" -ItemType "directory"
+New-Item -Path ".\10_Packages" -Name "Angular" -ItemType "directory"
+New-Item -Path ".\10_Packages" -Name "jQuery" -ItemType "directory"
+New-Item -Path ".\10_Packages" -Name "Nodejs" -ItemType "directory"
+New-Item -Path ".\10_Packages" -Name "Ember.js" -ItemType "directory"
+New-Item -Path ".\10_Packages" -Name "Lodash" -ItemType "directory"
+New-Item -Path ".\10_Packages" -Name "Meteor" -ItemType "directory"
+New-Item -Path ".\10_Packages" -Name "Dojo_Toolkit" -ItemType "directory"
